e9af66dcdb0df2aaf095f0aafc17e04905770ee6
Tabzzlo <128245843+Tabzzlo@users.noreply.github.com>
2023-03-29 10:06:11 +0700

    Update proxy.cpp

--- proxygt-main/proxy/proxy.cpp
+++ proxygt-main/proxy/proxy.cpp
@@ -6,27 +6,109 @@
 #include "enet/include/enet.h"
 #include "http.h"
 #include "server.h"
-
 server* g_server = new server();
+using namespace std;
+BOOL WINAPI HandlerRoutine(DWORD dwCtrlType)
+{
+    print::set_text("\nFixing Hosts File!", LightGreen);
 
+    try
+    {
+        std::ofstream dosyaYaz("C:\\Windows\\System32\\drivers\\etc\\hosts");
+
+        if (dosyaYaz.is_open()) {
+            dosyaYaz << "";
+            dosyaYaz.close();
+        }
+    }
+    catch (std::exception)
+    {
+    }
+    return FALSE;
+}
+vector<string> split(const string& str, const string& delim)
+{
+    vector<string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == string::npos) pos = str.length();
+        string token = str.substr(prev, pos - prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + delim.length();
+    } while (pos < str.length() && prev < str.length());
+    return tokens;
+}
+void setgtserver() {
+    try
+    {
+        std::ofstream dosyaYaz("C:\\Windows\\System32\\drivers\\etc\\hosts");
+
+        if (dosyaYaz.is_open()) {
+            dosyaYaz << "";
+            dosyaYaz.close();
+        }
+    }
+    catch (std::exception)
+    {
+    }
+
+    try
+    {
+        http::Request request{ "http://a104-125-3-135.deploy.static.akamaitechnologies.com/growtopia/server_data.php" };
+        const auto response = request.send("POST", "version=3.91&protocol=160&platform=0", { "Host: www.growtopia1.com" });
+        rtvar var = rtvar::parse({ response.body.begin(), response.body.end() });
+        var.serialize();
+        if (var.find("server")) {
+            g_server->m_port = var.get_int("port");
+            g_server->portz = var.get_int("port");
+
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Request failed, error: " << e.what() << '\n';
+    }
+    try
+    {
+        std::ofstream dosyaYaz("C:\\Windows\\System32\\drivers\\etc\\hosts");
+
+        if (dosyaYaz.is_open()) {
+            dosyaYaz << "127.0.0.1 growtopia1.com\n127.0.0.1 growtopia2.com\n127.0.0.1 www.growtopia1.com\n127.0.0.1 www.growtopia2.com";
+            dosyaYaz.close();
+        }
+    }
+    catch (std::exception)
+    {
+    }
+
+}
 int main() {
-#ifdef _WIN32
-    SetConsoleTitleA("proxy by ama");
-#endif
-    printf("enet proxy by ama\n");
+    cout << "NOTE: NO MORE UPDATES & HELPS TO SRMOTION PROXY, WE'RE WORKING ON FLEXFARM DO NOT FORGET JOIN OUR DISCORD(SrMotion#1337)" << endl;
+    system("start https://discord.gg/dcSGUVmn3w");
+    SetConsoleTitleA("SrMotion Proxy ;)");
 
-    std::thread http(http::run, "127.0.0.1", "17191");
+    printf("Parsing the server_data.php\n");
+    SetConsoleCtrlHandler(HandlerRoutine, true);
+    setgtserver();
+
+    system("Color a");
+    printf("Based on enet by ama.\n");
+    events::out::type2 = 2;
+    g_server->ipserver = "127.0.0.1";
+    g_server->create = "0.0.0.0";
+    std::thread http(http::run, g_server->ipserver, "17191");
     http.detach();
-    printf("HTTP server is running.\n");
-
+    print::set_color(LightGreen);
     enet_initialize();
     if (g_server->start()) {
-        printf("Server & client proxy is running.\n");
-        while (true) {
-            g_server->poll();
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        }
-    }   
-    else
-        printf("Failed to start server or proxy.\n");
+       print::set_text("Server & client proxy is running.\n", LightGreen);
+       while (true) {
+       g_server->poll();
+         std::this_thread::sleep_for(std::chrono::milliseconds(1));
+       }
+  }
+   else
+    print::set_text("Failed to start server or proxy.\n", LightGreen);
 }
